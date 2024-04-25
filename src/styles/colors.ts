62ab1c0c22fde0d37c13f9dcef6547a3a6d62890
Roshan-0001 <roshan.sharma6383@gmail.com>
2024-04-25 10:52:28 +0530

    .

---
+++ src/styles/colors.ts
@@ -0,0 +1,23 @@
+
+const colors = {
+  primary: '#7df9ff',
+  primaryLighter: '#7acaf9',
+  textColor: '#ffffff',
+  textColorSecondary: '#2f5cbc',
+  background: '#141d2b',
+  backgroundDarker: '#111927',
+  backgroundLighter: '#1a2332',
+  bgShadowColor: '#0f1620',
+  fgShadowColor: '#456602',
+  primaryTransparent: '#9fef0012',
+
+  // Action Colors
+  info: '#04e4f4',
+  success: '#20e253',
+  warning: '#f6f000',
+  error: '#fca016',
+  danger: '#f80363',
+  neutral: '#272f4d',
+};
+
+export default colors;
